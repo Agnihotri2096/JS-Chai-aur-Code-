2a8e489804d8a92f3ffc60467199c7e7b3fd2540
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2025-06-07 17:08:29 +0000

    add comment to the input field

--- 1)Basics/variables,constants_&_let.js
+++ 1)Basics/variables,constants_&_let.js
@@ -4,4 +4,8 @@ var accountpassword = "123445";
 accountCity  = "Jaipur";
 // accountEmail = "agg@gmail.com"; This is not possible.
 console.table([accountId,accountEmail,accountpassword,accountCity]);
+/*
+Prefer not to use var, use let or const instead.Because var is function scoped and let/const are block scoped.
+Use const for variables that won't change, let for those that might.
+*/
 console.log(accountId,accountEmail,accountpassword,accountCity);
