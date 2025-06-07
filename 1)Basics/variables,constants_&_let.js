82c77bebb35e749f24f274199f77c22beff3cb9c
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2025-06-07 17:06:11 +0000

    const,let&var

---
+++ 1)Basics/variables,constants_&_let.js
@@ -0,0 +1,7 @@
+const accountId = 2;
+let accountEmail = "agni@gmail.com";
+var accountpassword = "123445";
+accountCity  = "Jaipur";
+// accountEmail = "agg@gmail.com"; This is not possible.
+console.table([accountId,accountEmail,accountpassword,accountCity]);
+console.log(accountId,accountEmail,accountpassword,accountCity);
