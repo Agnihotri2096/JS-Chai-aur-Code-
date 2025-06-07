f658e935788b616068e746dd109b87e67809c652
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2025-06-07 17:10:18 +0000

    undefined

--- 1)Basics/variables,constants_&_let.js
+++ 1)Basics/variables,constants_&_let.js
@@ -2,10 +2,11 @@ const accountId = 2;
 let accountEmail = "agni@gmail.com";
 var accountpassword = "123445";
 accountCity  = "Jaipur";
+let accountState;
 // accountEmail = "agg@gmail.com"; This is not possible.
-console.table([accountId,accountEmail,accountpassword,accountCity]);
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
 /*
 Prefer not to use var, use let or const instead.Because var is function scoped and let/const are block scoped.
 Use const for variables that won't change, let for those that might.
 */
-console.log(accountId,accountEmail,accountpassword,accountCity);
+console.log(accountId,accountEmail,accountpassword,accountCity,accountState);
