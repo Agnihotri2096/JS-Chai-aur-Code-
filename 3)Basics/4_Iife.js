7036276b995109828481a87f406dc96ed182dcd7
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2025-06-13 15:51:57 +0000

    iife

---
+++ 3)Basics/4_Iife.js
@@ -0,0 +1,5 @@
+//Immediately invoked function expression (IIFE) is a JavaScript function that runs as soon as it is defined.
+(function() {
+    console.log("This function runs immediately!");
+})();
+(function(){})();
